e410accb3e34081bc7c4f947c60e8dbcfaf3a958
Augusto Meireles <augustomeireles05@gmail.com>
2022-02-10 22:32:02 -0300

    Atualizacao da modelagem

--- Dashboard/src/main/java/br/com/controller/ServletFornecedor.java
+++ Dashboard/src/main/java/br/com/controller/ServletFornecedor.java
@@ -1,41 +1,134 @@
 package br.com.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ServletFornecedor
- */
+import br.com.dao.FornecedorDAO;
+import br.com.entidade.Fornecedor;
+
+
 @WebServlet("/ServletFornecedor")
 public class ServletFornecedor extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+      
+	public FornecedorDAO fornec;
+
     public ServletFornecedor() {
         super();
-        // TODO Auto-generated constructor stub
+        this.fornec = new FornecedorDAO();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		String optionFornecedor = request.getParameter("optionFornecedor");
+		if (optionFornecedor == null) {
+			optionFornecedor = "";
+		}
+//		 System.out.println("doPost =" + optionFornecedor);
+		 
+		switch(optionFornecedor) {
+			case ("insertFormSupplier"):
+				showInsertSupplier(request, response);
+				break;
+			case ("updateFormSupplier"):
+				showUpdateSuppliers(request, response);
+				break;
+			case ("updateSupplier"):
+				updateSupplier(request, response);
+				break;
+			case ("deleteSupplier"):
+				deleteSupplier(request, response);
+				break;
+			case ("insertSupplier"):
+				insertSupplier(request, response);
+				break;
+			default:
+				selectAllSuppliers(request, response);
+		}
+		
 	}
 
+	private void showInsertSupplier(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+//		System.out.println("Qualquer coisa");
+		request.getRequestDispatcher("formFornecedor.jsp").forward(request, response);
+	}
+	
+	private void showUpdateSuppliers(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		Integer cod_fornecedor = Integer.parseInt(request.getParameter("cod_fornecedor"));
+		Fornecedor f = fornec.buscarSupplier(cod_fornecedor);
+		
+		request.setAttribute("supplier", f);
+		
+		request.getRequestDispatcher("formFornecedor.jsp").forward(request, response);
+	}
+	
+	private void insertSupplier(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		
+//		 System.out.println("qqr coisa 2");
+		String cnpjBack = request.getParameter("cnpj");
+		String razao_socialBack = request.getParameter("razao_social");
+		String inscricao_estadualBack = request.getParameter("inscricao_estadual");
+		String email_fornecedorBack = request.getParameter("email_fornecedor");
+		String telefone_fornecedorBack = request.getParameter("telefone_fornecedor");
+		
+//		 System.out.println("qqr coisa 2 = " + cnpjBack);
+//		 System.out.println("qqr coisa 2 = " + razao_socialBack);
+//		 System.out.println("qqr coisa 2 = " + inscricao_estadualBack);
+//		 System.out.println("qqr coisa 2 = " + email_fornecedorBack);
+//		 System.out.println("qqr coisa 2 = " + telefone_fornecedorBack);
+		
+		if ((cnpjBack != null) && (razao_socialBack != null) && (inscricao_estadualBack != null) && (email_fornecedorBack != null) && (telefone_fornecedorBack != null)) {
+			if (!cnpjBack.equals("")){
+				Fornecedor supplier1 = new Fornecedor(cnpjBack, razao_socialBack, inscricao_estadualBack, email_fornecedorBack, telefone_fornecedorBack);
+				fornec.addSupplier(supplier1);
+			}
+		}
+		response.sendRedirect("ServletFornecedor");
+	}
+	
+	private void selectAllSuppliers(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		request.setAttribute("listSupplier", this.fornec.getListSupplier());
+		request.getRequestDispatcher("fornecedor.jsp").forward(request, response);
+	}
+	
+	private void deleteSupplier(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String cod_fornecedorBack = request.getParameter("cod_fornecedor");
+		
+		if (cod_fornecedorBack != null) {
+			Integer cod_fornecedor = Integer.parseInt(cod_fornecedorBack);
+			this.fornec.removeSupplier(cod_fornecedor);
+		}
+		response.sendRedirect("ServletFornecedor");
+	}
+	
+	private void updateSupplier(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String cnpjBack = request.getParameter("cnpj");
+		String razao_socialBack = request.getParameter("razao_social");
+		String inscricao_estadualBack = request.getParameter("inscricao_estadual");
+		String email_fornecedorBack = request.getParameter("email_fornecedor");
+		String telefone_fornecedorBack = request.getParameter("telefone_fornecedor");
+		String cod_fornecedorBack = request.getParameter("cod_fornecedor");
+		
+		if ((cnpjBack != null) && (razao_socialBack != null) && (inscricao_estadualBack != null) && (email_fornecedorBack != null) && (telefone_fornecedorBack != null) && (cod_fornecedorBack != null)) {
+			if (!cnpjBack.equals("")){
+				Integer cod_fornecedor = Integer.parseInt(cod_fornecedorBack);
+				Fornecedor supplier1 = new Fornecedor(cnpjBack, razao_socialBack, inscricao_estadualBack, email_fornecedorBack, telefone_fornecedorBack); // ERRO ESTAVA AQUI: INVERTER PAIS COM EMAIL E EMAIL COM PAIS. AGORA ESTÁ CERTO
+				supplier1.setCod_fornecedor(cod_fornecedor);
+				this.fornec.updateSupplier(supplier1);
+			}
+		}
+		response.sendRedirect("ServletFornecedor");
+	}
+	
 }
